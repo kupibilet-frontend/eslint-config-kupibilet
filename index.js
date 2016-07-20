23b40baf290335fff39c54cc219e5a4285d4a844
Anton Fedyanin <anton@Antons-MacBook-Pro.local>
2016-07-20 16:49:25 +0300

    Linter is here, motherfucka

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: 'eslint-config-airbnb',
+  rules: {
+    semi: [2, 'never'],
+    quotes: [2, 'backtick'],
+    'space-before-function-paren': [2, 'always'],
+    'func-names': 0,
+    'arrow-parens': [2, 'always'],
+  },
+}
