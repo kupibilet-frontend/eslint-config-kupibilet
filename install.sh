78829df49e5293d833631a8267731355a9f4f03e
toshafed <krewger@yandex.ru>
2016-08-02 11:50:25 +0300

    update script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-PKG=eslint-config-airbnb npm info "$PKG" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev "$PKG"
+(
+  export PKG=eslint-config-airbnb;
+  npm info "$PKG" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev "$PKG"
+)
