70d5f563aa72e2c0d7613c371fd6f4e7a8e862cc
Valentin Semirulnik <v7rulnik@gmail.com>
2016-08-03 19:07:30 +0300

    Don't save eslint-config-airbnb

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 (
   export PKG=eslint-config-airbnb;
-  npm info "$PKG" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev "$PKG"
+  npm info "$PKG" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev
 )
