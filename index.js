c84f791e26e15a01c83076cffc0fe5df3dc8a7e1
toshafed <krewger@yandex.ru>
2016-08-10 01:44:02 +0300

    remove space-before-function rule

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = {
   parser: 'babel-eslint',
   rules: {
     semi: [2, 'never'],
-    'space-before-function-paren': [2, 'always'],
     'func-names': 0,
     'arrow-parens': [2, 'always'],
     'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
