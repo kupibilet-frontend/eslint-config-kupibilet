ad29d402ca4a1f0c06d8e7458cbe345ac371f436
Valentin Semirulnik <v7rulnik@gmail.com>
2016-08-10 12:47:03 +0300

    Save to deps instead devDeps

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 (
   export PKG=eslint-config-airbnb;
-  npm info "$PKG" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev
+  npm info "$PKG" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save
 )
