65001d1cbed967b3dc5ce87326db519ece223e72
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2019-02-26 17:44:57 +0300

    Add tests for Flow

---
+++ tests/flow-linting.js
@@ -0,0 +1,56 @@
+const { CLIEngine } = require('eslint')
+
+const cli = new CLIEngine({ ignore: false })
+
+const invalidFlowTypeDefinition = `
+/* eslint-disable no-console */
+// @flow
+type Author = {
+  name: string,
+  age:number
+}
+
+const author: Author = {
+  name: 'Vova',
+  age: 20,
+}
+
+console.log(author)
+`
+
+const validFlowTypeDefinition = `
+/* eslint-disable no-console */
+// @flow
+type Author = {
+  name: string,
+  age: number,
+}
+
+const author: Author = {
+  name: 'Vova',
+  age: 20,
+}
+
+console.log(author)
+`
+
+const snippetWithoutFlow = `
+/* eslint-disable no-console */
+const a = 1
+console.log(a)
+`
+
+test('Lints invalid flow type', () => {
+  const { results } = cli.executeOnText(invalidFlowTypeDefinition)
+  expect(results[0].messages).toHaveLength(1)
+})
+
+test('Flow does not throw errors if there are no errors', () => {
+  const { results } = cli.executeOnText(validFlowTypeDefinition)
+  expect(results[0].messages).toHaveLength(0)
+})
+
+test('Files without flow are OK', () => {
+  const { results } = cli.executeOnText(snippetWithoutFlow)
+  expect(results[0].messages).toHaveLength(0)
+})
