2c5e4a67f79f89fff04e7db725df67fd203037af
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2019-03-27 18:11:34 +0300

    Lint flow.js (#41)
    * Lint flow.js
    
    * Add tests for Flow
    
    * Do not warn uncovered code
    
    * Add tests for uncovered by flow code
    
    * Fix style of yml file
    
    * Refactor flow plugin recommended config extendtions
    
    * Add .idea to .gitignore
    
    * Warn instead of errors flow rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,7 @@ const rules = [
   './rules/functions.yml',
   './rules/imports.yml',
   './rules/react-jsx.yml',
+  './rules/flow.yml',
 ]
 
 module.exports = {
@@ -12,11 +13,19 @@ module.exports = {
     ...rules.map(require.resolve),
   ],
   parser: 'babel-eslint',
+  plugins: [
+    'flowtype',
+  ],
   env: {
     browser: true,
     jest: true,
   },
   globals: {
     Generator: true,
+  },
+  settings: {
+    flowtype: {
+      onlyFilesWithFlowAnnotation: true,
+    },
   },
 }
---
+++ tests/flow-linting.js
@@ -0,0 +1,75 @@
+const { CLIEngine } = require('eslint')
+
+const cli = new CLIEngine({ ignore: false })
+
+function getWarningsCount(messages) {
+  return messages.filter(({ severity }) => severity === 1).length
+}
+
+const invalidFlowTypeDefinition = `
+/* eslint-disable no-console */
+// @flow
+type Author = {
+  name: string,
+  age:number
+}
+
+const author: Author = {
+  name: 'Vova',
+  age: 20,
+}
+
+console.log(author)
+`
+
+const validFlowTypeDefinition = `
+/* eslint-disable no-console */
+// @flow
+type Author = {
+  name: string,
+  age: number,
+}
+
+const author: Author = {
+  name: 'Vova',
+  age: 20,
+}
+
+console.log(author)
+`
+
+const snippetWithoutFlow = `
+/* eslint-disable no-console */
+const a = 1
+console.log(a)
+`
+
+const fileWithUncoveredByFlowCode = `
+/* eslint-disable no-console */
+const a: number = 1
+const b = 2
+
+console.log(a, b)
+`
+
+test('Lints invalid flow type', () => {
+  const { results } = cli.executeOnText(invalidFlowTypeDefinition)
+
+  expect(getWarningsCount(results[0].messages)).toBe(1)
+})
+
+test('Flow does not throw errors if there are no errors', () => {
+  const { results } = cli.executeOnText(validFlowTypeDefinition)
+
+  expect(results[0].messages).toHaveLength(0)
+})
+
+test('Files without flow are OK', () => {
+  const { results } = cli.executeOnText(snippetWithoutFlow)
+  expect(results[0].messages).toHaveLength(0)
+})
+
+test('Files with some uncovered by flow code are OK', () => {
+  const { results } = cli.executeOnText(fileWithUncoveredByFlowCode)
+  expect(results[0].messages).toHaveLength(0)
+})
